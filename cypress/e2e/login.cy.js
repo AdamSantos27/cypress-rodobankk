1387b3d552473f13226d01067e8d8d2b734d0ba6
AdamSantos27 <Adam Santos>
2023-06-09 18:20:10 -0300

    commit inicial

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,10 @@
+describe('login Rodobank', () => {
+  it('fazer, login com sucesso', () => {
+    cy.visit('https://dev.rodobank.com.br')
+    cy.get('.user').type('adam.santos.dev@gerencial.com.br')
+    cy.get('.lock').type('235689')
+    cy.get('.form-group > .btn').click()
+    cy.wait(12000)
+    cy.get('.account-avatar').should('be.visible')
+  })
+})
