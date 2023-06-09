844fa1a5ca8d8df3d4a470b40a4e187fd7301615
AdamSantos27 <Adam Santos>
2023-06-09 19:01:30 -0300

    alteração no nome do teste

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
 describe('login Rodobank', () => {
-  it('fazer, login com sucesso', () => {
+  it('fazer, login com sucesso na plataforma', () => {
     cy.visit('https://dev.rodobank.com.br')
     cy.get('.user').type('adam.santos.dev@gerencial.com.br')
     cy.get('.lock').type('235689')
