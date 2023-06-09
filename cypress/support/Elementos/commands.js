8e6060306b0a92302e14ae630de0a0cf13f06193
AdamSantos27 <Adam Santos>
2023-06-09 19:41:18 -0300

    foi incluido comandos globais

--- cypress/support/Elementos/commands.js
+++ cypress/support/Elementos/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const Login = require('../').ELEMENTS
+
+Cypress.Commands.add('login', (email, password) => {   
+    cy.get(Login.campoEmail).type(email)
+    cy.get(Login.campoSenha).type(password)
+    cy.get(Login.botaoLogin).click()
+ })
