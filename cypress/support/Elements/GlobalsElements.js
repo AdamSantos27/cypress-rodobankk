af8d298a618411a3e5d96bfc9bf4d16e5e859e4b
AdamSantos27 <Adam Santos>
2023-06-12 12:59:20 -0300

    alteração da url

--- cypress/support/Elements/GlobalsElements.js
+++ cypress/support/Elements/GlobalsElements.js
@@ -1,6 +1,6 @@
 export const ELEMENTS = {
   // elementos referentes a tela de login
-  Url:'https://dev.rodobank.com.br',
+  Url:'https://localhost:8090',
   campoEmail:'.user',
   campoSenha:'.lock',
   botaoLogin:'.form-group > .btn'
